7db6a7609b888214c45e3d6dc08ab3e8341e3879
sintongsimon <54362199+sintongsimon@users.noreply.github.com>
2026-01-23 18:40:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ with nav3:
                                         marker_colors=color, textinfo='label+percent', 
                                         hoverinfo='value', hole=0.3))
     if not neg_df.empty and not pos_df.empty:
-        fig_sentiment.add_trace(go.Pie(labels=['Positive', 'Negative'], values=Label,
+        fig_sentiment.add_trace(go.Pie(labels=['Positive', 'Negative'], values=Sentimen,
                                       marker_colors=color_custom, textinfo='label+percent',
                                       hoverinfo='value', hole=0.3))
         
