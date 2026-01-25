ffe7dee249fe5a099d48b18af000961cbcbee6be
sintongsimon <54362199+sintongsimon@users.noreply.github.com>
2026-01-25 23:29:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,8 +86,7 @@ if st.session_state.rows_per_page != st.session_state.last_rows_per_page:
 with tab1:
     rows_per_page = st.session_state.rows_per_page
     total_rows = len(df_selection)
-    total_pages = max(1, (total_rows + rows_per_page - 1))
-    st.markdown(f"**Total Pages{total_pages}**")
+    total_pages = max(1, (total_rows + rows_per_page - 1) // rows_per_page)
 
     if st.session_state.nav_action == "first":
         st.session_state.page = 1
